83e0cdb655c66ca58543b9b957a57f2871c1ce11
Clinton Poole <clinton@cloudzone.co.za>
2020-07-21 21:59:36 +0200

    Made changes?

--- src/app/address/address.module.ts
+++ src/app/address/address.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AddressComponent } from './address.component';
-import {MaterialModule} from '../app.material.module';
+import { MaterialModule } from '../app.material.module';
 import { routing } from './address.routing';
 import { SnowModule } from '../shared/snow/snow.module';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
