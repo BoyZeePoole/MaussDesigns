8b032e0eb448b3dd3f5eb282cfd2a8c774a6e758
clinton Poole <clinton@cloudzone.co.za>
2021-04-17 16:47:24 +0200

    Added PrimeNG

--- src/app/services/settings.ts
+++ src/app/services/settings.ts
@@ -1,106 +1,106 @@
 export const EndPoints = {
-    userController : {
-      authenticate: '/users/authenticate',
-      getAllUsers: '/users/GetAll',
-      userRegister: '/users/Register',  
-      login: 'api/login',
-      getUserById: '/users/GetUserByUserId',
-      register: '/users/register',
-      updateAccount: '/users/updateaccount',
-      update: '/users/Update/',
-      delete: '/users/Delete/',
-      getMenu: 'users/GetMenuByUserId'
-    },
-    clientProfileController : {
-      getAllClients: '/api/ClientProfile/GetAll',
-      save: '/api/ClientProfile/Add',  
-      getClientById: '/api/ClientProfile/GetById/',
-      update: '/api/ClientProfile/Update/',
-      delete: '/api/ClientProfile/Delete/'
-    },
-    productController : {
-      getAllProductss: 'api/Products/get',
-      save: 'api/Products/add',  
-      getProductById: 'api/Products/GetById/',
-      update: 'api/Products/Update/',
-      delete: 'api/Products/Delete/',
-      getProductByTag: 'api/Products/getByTag',
-      getProductsByGroupId: 'api/Products/getbygroup',
-      getMoblieOptions: 'api/Products/getmobileoptions'
-    },
-    groupController : {
-      get : 'api/Group/getgroup'
-    },
-    subscribeController : {
-      save : 'api/ClientSubscription/add'
-    },
-    entityController : {
-      getEntityByPageName : 'api/entity/getentity'
-    },
-    formController : {
-      getForm : 'api/form/getform'
-    },    
-    orderController : {
-      save : 'api/Order/add',
-      getByUserId : 'api/Order/getByUserId'
-    },
-    customizeController : {
-      save : 'api/Customize/add'
-    },
-    wishlistController : {
-      add : 'api/Wishlist/add',
-      getAll : 'api/Wishlist/getall',
-      delete : 'api/Wishlist/delete',
-    },
-    colorWheelController : {
-      getAll: 'api/ColorWheel/get',
-      save : 'api/ColorWheel/add',
-      delete: 'api/ColorWheel/delete'
-    },
-    addressController : {
-      getAddressById: 'api/Address/getByUserId',
-      save: 'api/Address/add'
-    },
-    verifyController : {
-      verifyUser: 'api/Verify/VerifyUser'
-    }
+  userController: {
+    authenticate: '/users/authenticate',
+    getAllUsers: '/users/GetAll',
+    userRegister: '/users/Register',
+    login: 'api/login',
+    getUserById: '/users/GetUserByUserId',
+    register: '/users/register',
+    updateAccount: '/users/updateaccount',
+    update: '/users/Update/',
+    delete: '/users/Delete/',
+    getMenu: 'users/GetMenuByUserId'
+  },
+  clientProfileController: {
+    getAllClients: '/api/ClientProfile/GetAll',
+    save: '/api/ClientProfile/Add',
+    getClientById: '/api/ClientProfile/GetById/',
+    update: '/api/ClientProfile/Update/',
+    delete: '/api/ClientProfile/Delete/'
+  },
+  productController: {
+    getAllProductss: 'api/Products/get',
+    save: 'api/Products/add',
+    getProductById: 'api/Products/GetById/',
+    update: 'api/Products/Update/',
+    delete: 'api/Products/Delete/',
+    getProductByTag: 'api/Products/getByTag',
+    getProductsByGroupId: 'api/Products/getbygroup',
+    getMoblieOptions: 'api/Products/getmobileoptions'
+  },
+  groupController: {
+    get: 'api/Group/getgroup'
+  },
+  subscribeController: {
+    save: 'api/ClientSubscription/add'
+  },
+  entityController: {
+    getEntityByPageName: 'api/entity/getentity'
+  },
+  formController: {
+    getForm: 'api/form/getform'
+  },
+  orderController: {
+    save: 'api/Order/add',
+    getByUserId: 'api/Order/getByUserId'
+  },
+  customizeController: {
+    save: 'api/Customize/add'
+  },
+  wishlistController: {
+    add: 'api/Wishlist/add',
+    getAll: 'api/Wishlist/getall',
+    delete: 'api/Wishlist/delete',
+  },
+  colorWheelController: {
+    getAll: 'api/ColorWheel/get',
+    save: 'api/ColorWheel/add',
+    delete: 'api/ColorWheel/delete'
+  },
+  addressController: {
+    getAddressById: 'api/Address/getByUserId',
+    save: 'api/Address/add'
+  },
+  verifyController: {
+    verifyUser: 'api/Verify/VerifyUser'
+  }
 
 };
 
 export const MenuRoutes: Object[] = [{
-    icon: 'icon-squirrel',
-    route: '/home/splash',
-    title: 'About',
-    parentId: null,
-    sequence: 1,
-    refId: 2
-  }, 
-  {
-    icon: 'icon-pig',
-    route: '',
-    parentId: null,
-    title: 'Kindergarten',
-    sequence: 2,
-    refId: 10008
-  }, 
-  {
-    icon: 'icon-forward',
-    route: '/home/dashboard/1',
-    title: 'Gallery',
-    parentId: 10008,
-    sequence: 3,
-    submenu: 1,
-    refId: 3
-  }, 
-  {
-    icon: 'icon-forward',
-    route: '/home/customize',
-    title: 'Create your own mobile',
-    parentId: 10008,
-    sequence: 4,
-    submenu: 1,
-    refId: 10011
-  }, 
+  icon: 'icon-squirrel',
+  route: '/home/splash',
+  title: 'About',
+  parentId: null,
+  sequence: 1,
+  refId: 2
+},
+{
+  icon: 'icon-pig',
+  route: '/home/dashboard/1',
+  parentId: null,
+  title: 'Bespoke Mobiles',
+  sequence: 2,
+  refId: 10008
+},
+{
+  icon: 'icon-butterfly',
+  route: '/home/dashboard/1',
+  title: 'D.I.Y Shop',
+  parentId: null,
+  sequence: 3,
+  submenu: 0,
+  refId: 3
+},
+  // {
+  //   icon: 'icon-forward',
+  //   route: '/home/customize',
+  //   title: 'Create your own mobile',
+  //   parentId: 10008,
+  //   sequence: 4,
+  //   submenu: 1,
+  //   refId: 10011
+  // }, 
   // {
   //   icon: 'icon-butterfly',
   //   route: '',
@@ -138,5 +138,5 @@ export const MenuRoutes: Object[] = [{
 ];
 
 export const ApplicationSettings = {
-  Title: 'Mauss Designs'   
- };
+  Title: 'Mauss Designs'
+};
